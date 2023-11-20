cb87113dc1cca1eda657c080aee6cf63ce688557
iamdudumon <kksy4511@naver.com>
2023-11-20 14:23:52 +0900

    feat: 게이트웨이 기본 데이터(온도, 습도, 이슬점) 쿼리해서 dataframe으로 생성 후 csv 파일로 저장 기능 추가

--- influxdb/gateway_data_to_csv.py
+++ influxdb/gateway_data_to_csv.py
@@ -1,48 +1,54 @@
 import influxdb_client
-import json
+import pandas as pd
 
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-# Initialize InfluxDB client
-token = "blQNLEOIYKlbJ73tWhurIedUOGQSxtfR0eHPGjPlF0LPWxi92dFuCcWwv1K0JoXjHdHiRCqiSHZfihv-PPJfWw=="
+token = "1cDBU-BVSjJ0emb2KmZjgOhGNs3V1-cW60aqfMHoaeyndLVVdFfIo_Gp81OZn1iZ6gCHUvTjoi2rheflojizOg=="
 org = "Brighten"
-url = "http://localhost:8086"
-bucket = "Sensor_test"
+url = "http://211.57.200.6:8086"
+bucket = "smarthome"
+
+
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 query_api = client.query_api()
 
-query = f"""
-            import "json"
+for mac_address in ["W220_8929E8", "W220_D6FC80", "W220_818FB4", "W220_BADBD8"]:
+    # dataframe 생성
+    df = pd.DataFrame(columns=["date", "mac_address", "temperature", "humidity", "dew_point"])
 
-            from(bucket: "Sensor_test")
-                |> range(start: -1y)
-                |> filter(fn: (r) => r._measurement == \"GatewayData\")
-                |> filter(fn: (r) => r._field == \"humidity\")
-                |> window(every: 30m)
-                |> mean()
-                |> map(fn: (r) => ({"{"}r with jsonStr: string(v: json.encode(v: {"{"}"measurement": r._measurement, "r._field": r._field, "mac_address": r.mac_address, "date": r._start, "value": r._value{"}"})){"}"}))
-            """
-# |> filter(fn: (r) => r[\"mac_address\"] == \"W220_D6FC80\")
-# Execute the query and fetch the result
+    query = f"""
+                from(bucket: "{bucket}")
+                    |> range(start: 0)
+                    |> filter(fn: (r) => r._measurement == \"GatewayData\")
+                    |> filter(fn: (r) => r._field == \"temperature\" or r._field == \"humidity\" or r._field == \"dew_point\")
+                    |> filter(fn: (r) => r[\"mac_address\"] == "{mac_address}")
+                    |> window(every: 30m)
+                    |> mean()
 
-result = query_api.query(query, org=org)
-# json으로 파싱 -> > 출력 재지정자로 바로 .json 파일로 저장
-print("{")
-i = 0
-for table in result:
-    for record in table.records:
-        print('"' + str(i) + '"' + ": ")
-        data = json.loads(record.__getitem__('jsonStr'))
-        print(data, end='')
-        print(",")
-        i += 1
-print("}")
+                """
 
-# # Convert the result to a DataFrame
-# df = pd.DataFrame(result['Sensor_test'])
+    result = query_api.query(query, org=org)
 
-# # Save the DataFrame to a CSV file
-# df.to_csv('./sensor.csv', index=False)
+    data = []
+    data_dict = {}
+
+    for table in result:
+        for record in table.records:
+            date = record["_start"]
+            mac_address = record["mac_address"]
+            field = record["_field"]
+            value = record["_value"]
+            if not (date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address) in data_dict:
+                data_dict[date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address] = {}
+            
+            data_dict[date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address][field] = value
+            # data.append({"date": date, "mac_address": mac_address, field: value})
+
+    # 데이터프레임 생성
+    for key, value in data_dict.items():
+        date = key.split("~")[0]
+        mac_address = key.split("~")[1]
+        data.append({"date": date, "mac_address": mac_address, "temperature": value["temperature"], "humidity": value["humidity"], "dew_point": value["dew_point"]})
+    
+    df = pd.DataFrame(data)
+   
+    df.to_csv(f"./data/gateway/{mac_address}.csv", index=False)
