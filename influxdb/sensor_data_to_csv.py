ca22151d2ce6d666307e1139b721b0fdb2efe47b
iamdudumon <kksy4511@naver.com>
2023-11-20 14:36:13 +0900

    refactor: sensor 코드 잠시 보류

--- influxdb/sensor_data_to_csv.py
+++ influxdb/sensor_data_to_csv.py
@@ -1,68 +1,41 @@
 # import influxdb_client
-# import pandas as pd
+# import json
 
+# # Initialize InfluxDB client
 # token = "1cDBU-BVSjJ0emb2KmZjgOhGNs3V1-cW60aqfMHoaeyndLVVdFfIo_Gp81OZn1iZ6gCHUvTjoi2rheflojizOg=="
 # org = "Brighten"
 # url = "http://211.57.200.6:8086"
 # bucket = "smarthome"
 
-
 # client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 # query_api = client.query_api()
-
-
-# # dataframe 생성
-# device_type = "door"
-# # df = pd.DataFrame(columns=["date", "mac_address", f"{device_type}1", f"{device_type}2", f"{device_type}3"])
-
+# # Define the query
 # query = f"""
-#             from(bucket: "smarthome")
-#                 |> range(start: 0)
-#                 |> filter(fn: (r) => r["_measurement"] == "SensorData")
-#                 |> filter(fn: (r) => r["mac_address"] == "W220_D6FC80")
-#                 |> filter(fn: (r) => r["Device"] == "0x5722" or r["Device"] == "0x838A" or r["Device"] == "0xC5FF")
-#                 |> filter(fn: (r) => r["_field"] == "Contact")
-#                 |> aggregateWindow(every: 30m, fn: count, createEmpty: false)
+#         import "json"
+#         import "experimental"
 
-#             """
-
+#             from(bucket: "Sensor_test")
+#                 |> range(start: 2023-08-22T00:00:00Z)
+#                 |> filter(fn: (r) => r._measurement == \"SensorData\")
+#                 |> filter(fn: (r) => r[\"mac_address\"] == \"W220_818FB4\")
+#                 |> filter(fn: (r) => r[\"Device\"] == \"0x5D36\" or r[\"Device\"] == \"0x7022\" or r[\"Device\"] == \"0x8015\" or r[\"Device\"] == \"0xE30E\")
+#                 |> aggregateWindow(every: 30m, fn: count, createEmpty: true)
+#                 |> fill(column: "_value", value: 0)
+#                 |> map(fn: (r) => ({"{"}r with jsonStr: string(v: json.encode(v: {"{"}"measurement": r._measurement, "r.Device": r.Device, "mac_address": r.mac_address, "date": r._time, "count": r._value{"}"})){"}"}))
+#         """
+# # or r[\"Device\"] == \"0x5D36\" or r[\"Device\"] == \"0x7022\" or r[\"Device\"] == \"0x8015\" or r[\"Device\"] == \"0xE30E\"
+# # r[\"Device\"] == \"0xF26E\" or r[\"Device\"] == \"0xC5FF\"
+# # Execute the query and fetch the result
 # result = query_api.query(query, org=org)
 
-# data = []
-# data_dict = {}
-
-# door_col = {
-#     "0x5722": "door1",
-#     "0x838A": "door2",
-#     "0xC5FF": "door3"
-# }
-
+# # json으로 파싱 -> > 출력 재지정자로 바로 .json 파일로 저장
+# print("{")
+# i = 0
 # for table in result:
 #     for record in table.records:
-#         date = record["_time"]
-#         mac_address = record["mac_address"]
-#         device = record["Device"]
-#         value = record["_value"]
-#         if not (date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address) in data_dict:
-#             data_dict[date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address] = {}
-        
-#         data_dict[date.strftime("%Y-%m-%d %H:%M:%S") + "~" + mac_address][door_col[device]] = value
-#         # data.append({"date": date, "mac_address": mac_address, field: value})
-# # 데이터프레임 생성
-# for key, value in data_dict.items():
-#     date = key.split("~")[0]
-#     mac_address = key.split("~")[1]
-
-#     obj = {}
-#     obj['date'] = date;
-#     obj['mac_address'] = mac_address
-
-#     obj[f"{device_type}1"] =  value.get(f"{device_type}1", 0)
-#     obj[f"{device_type}2"] =  value.get(f"{device_type}2", 0)
-#     obj[f"{device_type}3"] =  value.get(f"{device_type}3", 0)
-
-#     data.append(obj)
-
-# df = pd.DataFrame(data)
-
-# df.to_csv(f"./data/{device_type}/{mac_address}_{device_type}.csv", index=False)
+#         print('"' + str(i) + '"' + ": ")
+#         data = json.loads(record.__getitem__("jsonStr"))
+#         print(json.dumps(data), end='')
+#         print(",")
+#         i += 1
+# print("}")
